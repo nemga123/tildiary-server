6c4f0e1415eba9ba57793fd5dfb9b6dc88441ec3
nemga123 <hspark0419@naver.com>
2023-03-02 16:01:37 +0900

    feat: isort, mypy, flake8

--- tildiary/users/models.py
+++ tildiary/users/models.py
@@ -1,9 +1,6 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    PermissionsMixin,
-    BaseUserManager
-)
 
 
 class UserManager(BaseUserManager):
@@ -15,15 +12,19 @@ class UserManager(BaseUserManager):
             email=self.normalize_email(email),
             username=username
         )
+
+        assert isinstance(user, User)
+
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, email, username, password=None):
         user = self.create_user(email, username, password)
         user.is_admin = True
         user.save(using=self._db)
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
